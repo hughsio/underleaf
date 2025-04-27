47067b1cd8bad48683057742d253a1c188bdb970
hughio <61617034+hughsio@users.noreply.github.com>
2025-04-27 14:43:57 -0400

    Initial Commit for Senior Project Folder

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Optional: cleanup
+rm -f report.{aux,bbl,bcf,blg,log,run.xml,toc}
+
+# Docker compile with biber
+docker run --rm \
+  -v "/Users/hugh/Documents/Senior Project I (FALL 2024)Final Report":/workdir \
+  -w /workdir \
+  texlive/texlive \
+  
+  /bin/bash -c "pdflatex report.tex && biber report && pdflatex report.tex && pdflatex report.tex"
